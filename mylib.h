9c942bc2e14be3be10edb8afa372a829d2832623
Pijussad <64306577+Pijussad@users.noreply.github.com>
2023-06-02 11:56:12 +0300

    Add files via upload

--- mylib.h
+++ mylib.h
@@ -41,15 +41,69 @@ using std::runtime_error;
 using std::to_string;
 
 
-class Studentas {
+class Zmogus
+{
+    protected:
+        string vardas1, pavarde1;
+    public:
+        Zmogus() : vardas1(""), pavarde1("") {}
+        Zmogus(string vardas, string pavarde) : vardas1(vardas), pavarde1(pavarde) {}
+        virtual string vardas() const = 0;
+        ~Zmogus() {}
+};
+
+class Studentas : public Zmogus
+{
 private:
-    string vardas1 = "", pavarde1 = "";
+    string vardas1 = "";
+    string pavarde1 = "";
     vector<int> paz1;
     int egz1 = 0;
 
 public:
     // Constructor
-    Studentas() {}
+    Studentas() : Zmogus(), egz1(0) {}
+    Studentas(string vardas, string pavarde, vector<int> paz, int egz)  : Zmogus(vardas,pavarde), paz1(paz), egz1(egz){}
+
+    // Destructor
+    ~Studentas() {paz1.clear();}
+
+    // Copy constructor
+    Studentas(const Studentas& other)
+        : vardas1(other.vardas1),
+          pavarde1(other.pavarde1),
+          paz1(other.paz1),
+          egz1(other.egz1) {}
+
+    // Copy assignment operator
+    Studentas& operator=(const Studentas& other) {
+        if (this != &other) {
+            vardas1 = other.vardas1;
+            pavarde1 = other.pavarde1;
+            paz1 = other.paz1;
+            egz1 = other.egz1;
+        }
+        return *this;
+    }
+
+    // Move constructor
+    Studentas(Studentas&& other)
+        : vardas1(std::move(other.vardas1)),
+          pavarde1(std::move(other.pavarde1)),
+          paz1(std::move(other.paz1)),
+    egz1(std::move(other.egz1)) {other.egz1=0;}
+
+    // Move assignment operator
+    Studentas& operator=(Studentas&& other) {
+        if (this != &other) {
+            vardas1 = std::move(other.vardas1);
+            pavarde1 = std::move(other.pavarde1);
+            paz1 = std::move(other.paz1);
+            egz1 = std::move(other.egz1);
+            other.egz1=0;
+        }
+        return *this;
+    }
 
     // Getters and Setters
     string vardas() const { return vardas1; }
@@ -63,7 +117,26 @@ public:
 
     int egz() const { return egz1; }
     void egz(int e) { egz1 = e; }
+    
+    Studentas operator+(const Studentas& other) const {
+            Studentas result;
+
+            // Combine the names
+            result.vardas1 = vardas1 + " " + other.vardas1;
+            result.pavarde1 = pavarde1 + " " + other.pavarde1;
+
+            // Combine the vectors of grades
+            result.paz1 = paz1;
+            result.paz1.insert(result.paz1.end(), other.paz1.begin(), other.paz1.end());
+
+            // Set the exam grade as the average of the two students
+            result.egz1 = (egz1 + other.egz1);
+
+            return result;
+        }
+
 };
+
 
 extern char pasirinkimas;
 void generateFile(int n);
